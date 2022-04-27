0fbae0848659300782abdb963a0290f9e7a422b3
M-Lalaina <lalaina2302@gmail.com>
2022-04-27 21:23:42 +0300

    📝 ...

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,18 @@
-// Récupération des cases à clicker
+// Récupérer les éléùents du DOM
+
 let items = [...document.getElementsByClassName("grid-item")];
-let player = document.getElementById("player");
+let YOU = document.getElementById("YOU");
+let CPU = document.getElementById("CPU");
 let myScore = document.getElementById("my-score");
 let pcScore = document.getElementById("pc-score");
 let restartGame = document.getElementsByClassName("restart-game");
 
+/*valeur par défaut des éléments*/
+
 let state = {
-    joueurEnCours: "YOU",
-    Me: 0,
-    pC: 0,
+    joueurEnCours: 1,
+    You: 0, //score du joueur
+    pC: 0, // score du CPU
     i1: 0,
     i2: 0,
     i3: 0,
@@ -20,8 +24,11 @@ let state = {
     i9: 0,
 };
 
+/**fonction reset
+ * Recommence le jeu,
+ * Vide toutes les cases et remet la valeur 0,
+ */
 const resetAllItems = () => {
-    state.i1 = 0;
     state.i2 = 0;
     state.i3 = 0;
     state.i4 = 0;
@@ -34,7 +41,7 @@ const resetAllItems = () => {
 
 //possibilités de victoires
 
-const verifyWinOrLose = () => {
+const verifyIfWinOrLose = () => {
     if (
         (state.i1 == state.i2 && state.i2 == state.i3 && state.i1 > 0) ||
         (state.i1 == state.i4 && state.i4 == state.i7 && state.i1 > 0) ||
@@ -62,31 +69,52 @@ const verifyWinOrLose = () => {
     }
 };
 
+// Fonctions qui se joue lorsqu'un joueur clique sur une des cases
+
 const jouerCase = (e) => {
     let idItem = e.target.id;
 
-    /* console.log(e);*/ //affiche tout les évènements possible danas chaque case
+    /* console.log(e);*/ // affiche tout les évènements possible danas chaque case
 
-    /* console.log(idItem);*/ //récupère les 'id' des case
+    //console.log(idItem); // récupère les 'id' des case
 
     if (state[idItem] !== 0) return;
 
     state[idItem] = state.joueurEnCours;
 
-    let WinTheGame = verifyWinOrLose();
+    let WinTheGame = verifyIfWinOrLose();
 
     if (WinTheGame === true) {
-        alert("Félicitation, vous avez gagné le jeu! ");
+        /******s'il ya un gagnant : (soit 'YOU', soit 'CPU')******/
+        if (state.joueurEnCours == 1) {
+            alert("Congrats You win the game! ");
+            //si le gagnant est 'YOU'
+            state.Me++;
+            myScore.textContent = state.Me;
+        } else {
+            alert("Oups, You lose!. ");
+            //si le gagnant est'CPU'
+            state.pC++;
+            pcScore.textContent = state.pC;
+        }
         resetAllItems();
-        items.forEach((item) => (item.textContent = ""));
+        items.forEach((c) => (c.textContent = "")); //pour vider le contenu des cases
     } else if (WinTheGame === null) {
+        /******si le match est null******/
         alert("Match null");
         resetAllItems();
-        items.forEach((item) => (item.textContent = ""));
+        items.forEach((c) => (c.textContent = ""));
     } else if (WinTheGame === false) {
-        alert("Oups, vous avez perdu. ");
+        /******Un coup Simple (on ne peut pas encore det s'y a un gagnant ou pas) ******/
+        if (state.joueurEnCours === 1) {
+            e.target.textContent = "X";
+            state.joueurEnCours = 2;
+        } else {
+            e.target.textContent = "O";
+            state.joueurEnCours = 1;
+        }
         resetAllItems();
-        items.forEach((item) => (item.textContent = ""));
+        items.forEach((c) => (c.textContent = ""));
     }
 };
 
